ede12c676b29a9562a35c3f901248c1e50b8791d
Denis Khmelnov <denishmelnov@gmail.com>
2022-07-09 12:54:32 +0500

    feat: implement get_all and get_by_pk functions

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
-import flask
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+
+def load_candidates():
+    with open("candidates.json") as file:
+        candidates_list = json.load(file)
+        return candidates_list
+
+
+def formate_candidate(candidate):
+    return f"""Имя кандидата - {candidate["name"]}
+Позиция кандидата - {candidate["position"]}
+Навыки кандидата: {candidate["skills"]}"""
+
+
+# Создаем список кандидатов
+candidates = load_candidates()
+
+
+@app.route("/")
+def get_all():
+    formatted_candidates = [formate_candidate(candidate) for candidate in candidates]
+    formatted_candidates = "\n\n".join(formatted_candidates)
+    return "".join(["<pre>", formatted_candidates, "</pre>"])
+
+
+@app.route("/candidates/<int:pk>/")
+def get_by_pk(pk):
+    for candidate in candidates:
+        if candidate["pk"] == pk:
+            return "".join(["<pre>", formate_candidate(candidate), "</pre>"])
+    return "Нет кандидата с таким PK"
+
+# `get_by_skill(skill_name)`, которая вернет кандидатов по навыку
+
+app.run()
